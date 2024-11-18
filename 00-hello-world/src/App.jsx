7251c77141847379e22bb0c164186d550ccd151a
DavidOlmos03 <juanda_201001@hotmail.com>
2024-11-18 12:38:46 -0500

    variables and functions as parameters

--- 00-hello-world/src/App.jsx
+++ 00-hello-world/src/App.jsx
@@ -1,24 +1,28 @@
+import { TwitterFollowCard } from "./TwitterFollowCarrd";
 export function App() {
+    const formatUserName = (userName) => `@${userName}`
   return (
-    <article className="tw-followCard">
-        <header className="tw-followCard-header">
-            <img 
-            className="tw-followCard-avatar" 
-            src="https://davidolmos-portfolio.netlify.app/assets/imgs/avatar.jpeg" 
-            alt="The avatar of Juan David Ruiz" 
-            />
-            <div className="tw-followCard-info">
-                <strong>Juan David Ruiz</strong>
-                <span 
-                    className="tw-followCard-infoUserName"
-                >@juandavidruiz</span>
-            </div>
-        </header>
-        <aside>
-            <button className="tw-followCard-button">
-                Follow
-            </button>
-        </aside>
-    </article>
+    <section className="App">
+       <TwitterFollowCard
+        formatUserName={formatUserName}
+        userName="davidolmos"
+        name="Juan David Ruiz"
+        isFollowing={false}
+        />
+       <TwitterFollowCard 
+        formatUserName={formatUserName}
+        userName="davidolmos"
+        name="Juan David Ruiz"
+        isFollowing
+        />
+       <TwitterFollowCard 
+        formatUserName={formatUserName}
+        userName="davidolmos"
+        name="Juan David Ruiz"
+        isFollowing={false}
+        />
+    
+    </section>
+  
   )
 }
---
+++ 00-hello-world/src/TwitterFollowCarrd.jsx
@@ -0,0 +1,25 @@
+export function TwitterFollowCard({formatUserName,userName, name, isFollowing}) {
+    const imgSrc = "https://davidolmos-portfolio.netlify.app/assets/imgs/avatar.jpeg" 
+    return (
+    <article className="tw-followCard">
+        <header className="tw-followCard-header">
+            <img 
+            className="tw-followCard-avatar" 
+            src = {imgSrc}  
+            alt="The avatar of Juan David Ruiz" 
+            />
+            <div className="tw-followCard-info">
+                <strong>{name}</strong>
+                <span 
+                    className="tw-followCard-infoUserName"
+                >{formatUserName(userName)}</span>
+            </div>
+        </header>
+        <aside>
+            <button className="tw-followCard-button">
+                Follow
+            </button>
+        </aside>
+    </article>
+  )
+}
